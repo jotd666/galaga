a864286d7d41f05d5d9e344974ca8aa4c6c184fb
Jean-François Fabre <jotd666@gmail.com>
2024-02-10 19:51:48 +0100

    mockup with moving enemies!

---
+++ assets/galaga_gfx.h
@@ -0,0 +1,11 @@
+#ifndef __GALAGA_GFX_H__
+#define __GALAGA_GFX_H__
+
+#define NUM_COLOURS          32
+#define NUM_FG_TILE_CLUTS    64
+#define NUM_SPRITE_CLUTS     64
+
+#define NUM_FG_TILES         256
+#define NUM_SPRITES          128
+
+#endif // __GALAGA_GFX_H__
