d9e7428dc9bdba20e25abc511948128ac9e8aa88
Jean-François Fabre <jotd666@gmail.com>
2024-05-26 22:58:53 +0200

    added credit sound

--- assets/amiga/convert_sounds.py
+++ assets/amiga/convert_sounds.py
@@ -25,45 +25,19 @@ vhq_sample_rate = 22050
 EMPTY_SND = "EMPTY_SND"
 sound_dict = {
 "CREDIT_SND"               :{"index":0,"channel":0,"sample_rate":hq_sample_rate},
-"DRAW_MAZE_SND"             :{"index":1,"pattern":1,"loops":True,"volume":32},
-"START_SND"              :{"index":2,"pattern":0,"loops":False,"volume":32},
-"DIAMONDS_ALIGNED_SND"               :{"index":3,"channel":0,"sample_rate":hq_sample_rate},
-"LEVEL_COMPLETED_SND"             :{"index":4,"pattern":9,"ticks":190,"loops":False,"volume":32},
-"PLAYER_DIES_SND"             :{"index":5,"pattern":10,"ticks":150,"loops":False,"volume":32},
-"INTERMISSION_MUSIC_SND"             :{"index":7,"pattern":11,"loops":False,"volume":32},
-"IN_GAME_MUSIC_SND"             :{"index":8,"pattern":2,"loops":True,"volume":32},
-"HISCORE_MUSIC_SND"             :{"index":9,"pattern":15,"loops":True,"volume":32},
-"IN_GAME_MUSIC_FAST_SND"             :{"index":10,"pattern":5,"loops":True,"volume":32},
-"IN_GAME_MUSIC_FASTER_SND"             :{"index":11,"pattern":8,"loops":True,"volume":32},
-
-
-# second channel (called by 18C7)
-"SNOBEE_EATEN_SND"               :{"index":12,"channel":2,"sample_rate":hq_sample_rate},
-"SNOBEE_CHICKEN_SND"               :{"index":13,"channel":2,"sample_rate":hq_sample_rate},
-"SNOBEE_HATCHING_SND"               :{"index":14,"channel":2,"sample_rate":hq_sample_rate},
-"SNOBEE_CRUSHED_SND"               :{"index":15,"channel":2,"sample_rate":hq_sample_rate},
-"SNOBEE_STUNNED_SND"               :{"index":16,"channel":2,"sample_rate":hq_sample_rate},
-"DIAMONDS_BONUS_SND"               :{"index":17,"channel":2,"loops":True,"sample_rate":vhq_sample_rate},
+#"HISCORE_MUSIC_SND"             :{"index":9,"pattern":15,"loops":True,"volume":32},
+#"IN_GAME_MUSIC_FAST_SND"             :{"index":10,"pattern":5,"loops":True,"volume":32},
+#"IN_GAME_MUSIC_FASTER_SND"             :{"index":11,"pattern":8,"loops":True,"volume":32},
 
 # third channel
-"EXTRA_LIFE_SND"               :{"index":18,"channel":3,"sample_rate":hq_sample_rate},
-"BLOCK_BROKEN_SND"               :{"index":19,"channel":3,"sample_rate":hq_sample_rate},
-"SHAKE_WALL_SND"               :{"index":20,"channel":3,"sample_rate":hq_sample_rate},
-"BLOCK_HITS_WALL_SND"               :{"index":21,"channel":3,"sample_rate":hq_sample_rate},
-"BLOCK_SLIDES_SND"               :{"index":22,"channel":3,"sample_rate":hq_sample_rate},
-
-}
-sound_dict_set_2 = {
-"IN_GAME_MUSIC_SND"             :{"index":8,"pattern":2+15,"loops":True,"volume":32},
-"IN_GAME_MUSIC_FAST_SND"             :{"index":10,"pattern":8+15,"loops":True,"volume":32},
-"IN_GAME_MUSIC_FASTER_SND"             :{"index":11,"pattern":14+15,"loops":True,"volume":32},
+"EXTRA_LIFE_SND"               :{"index":1,"channel":3,"sample_rate":hq_sample_rate},
 
 }
 
 max_sound = 0x20  # max(x["index"] for x in sound_dict.values())+1
 sound_table = [""]*max_sound
 sound_table_set_1 = ["\t.long\t0,0"]*max_sound
-sound_table_set_2 = ["\t.long\t0,0"]*max_sound
+
 
 
 
@@ -116,13 +90,12 @@ with open(sndfile,"w") as fst,open(outfile,"w") as fw:
 
     for wav_entry,details in sound_dict.items():
         sound_index = details["index"]
-        details_set2 = sound_dict_set_2.get(wav_entry,details)
+
         channel = details.get("channel")
         if channel is None:
             # if music loops, ticks are set to 1 so sound orders only can happen once (else music is started 50 times per second!!)
 
             sound_table_set_1[sound_index] = "\t.word\t{},{},{}\n\t.byte\t{},{}".format(2,details["pattern"],details.get("ticks",0),details["volume"],int(details["loops"]))
-            sound_table_set_2[sound_index] = "\t.word\t{},{},{}\n\t.byte\t{},{}".format(2,details_set2["pattern"],details_set2.get("ticks",0),details_set2["volume"],int(details_set2["loops"]))
         else:
             wav_name = os.path.basename(wav_entry).lower()[:-4]
             wav_file = os.path.join(sound_dir,wav_name+".wav")
@@ -152,7 +125,6 @@ with open(sndfile,"w") as fst,open(outfile,"w") as fw:
             wav = os.path.splitext(wav_name)[0]
             sound_table[sound_index] = "    SOUND_ENTRY {},{},{},{},{},{}\n".format(wav,len(signed_data)//2,channel,used_sampling_rate,int(64*amp_ratio),used_priority)
             sound_table_set_1[sound_index] = f"\t.word\t1,{int(details.get('loops',0))}\n\t.long\t{wav}_sound"
-            sound_table_set_2[sound_index] = sound_table_set_1[sound_index]
 
             if amp_ratio > 0:
                 maxed_contents = [int(x/amp_ratio) for x in signed_data]
@@ -181,21 +153,19 @@ with open(sndfile,"w") as fst,open(outfile,"w") as fw:
             write_asm(contents,fw)
 
     # make sure next section will be aligned
-    with open(os.path.join(sound_dir,f"{gamename}_conv.mod"),"rb") as f:
-        contents = f.read()
+##    with open(os.path.join(sound_dir,f"{gamename}_conv.mod"),"rb") as f:
+##        contents = f.read()
     fw.write("{}:".format(music_module_label))
-    write_asm(contents,fw)
+#    write_asm(contents,fw)
     fw.write("\t.align\t8\n")
 
 
     fst.writelines(sound_table)
-    fst.write("\n\t.global\t{0}\n\n{0}:\n".format("sound_table_set_1"))
+    fst.write("\n\t.global\t{0}\n\n{0}:\n".format("sound_table"))
     for i,st in enumerate(sound_table_set_1):
         fst.write(st)
         fst.write(" | {}\n\n".format(i))
-    fst.write("\n\t.global\t{0}\n\n{0}:\n".format("sound_table_set_2"))
-    for i,st in enumerate(sound_table_set_2):
-        fst.write(st)
-        fst.write(" | {}\n\n".format(i))
+
+
 
 
