a601067cc33a67962e828354ab6ab5b5a61febcf
Jean-François Fabre <jotd666@gmail.com>
2024-09-23 21:52:50 +0200

    updated build scripts

--- tools/create_amiga_archive.py
+++ tools/create_amiga_archive.py
@@ -9,6 +9,8 @@ os.environ["PATH"] += os.pathsep+r"K:\progs\cli"
 cmd_prefix = ["make","-f",os.path.join(progdir,"makefile.am")]
 
 subprocess.check_call(cmd_prefix+["clean"],cwd=os.path.join(progdir,"src"))
+for s in ["convert_sounds.py","convert_graphics.py"]:
+    subprocess.check_call(["cmd","/c",s],cwd=os.path.join(progdir,"assets","amiga"))
 
 subprocess.check_call(cmd_prefix+["RELEASE_BUILD=1"],cwd=os.path.join(progdir,"src"))
 # create archive
